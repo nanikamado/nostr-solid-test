8ea6b79a64f74ac41c674344497c6aec929d5215
Mado <nanikamado@gmail.com>
2025-03-13 16:27:42 +0900

    wip

--- src/Events.tsx
+++ src/Events.tsx
@@ -1,19 +1,16 @@
 import "./Events.css";
-import { onCleanup, onMount, For, Show, createEffect } from "solid-js";
-import { get_events_rev, NostrEvent } from "./nostr.ts";
+import { onCleanup, onMount, For, Show } from "solid-js";
+import { NostrEvent } from "./nostr.ts";
 // import { Channel } from "./channel.ts";
 import {
   createRxNostr,
   createRxForwardReq,
   createRxBackwardReq,
-  now,
-  EventPacket,
 } from "rx-nostr";
 // import * as Rxn from "rx-nostr";
 import { verifier } from "rx-nostr-crypto";
 import * as Rx from "rxjs";
 import { createStore } from "solid-js/store";
-import { trackDeep } from "@solid-primitives/deep";
 
 type EventSignal = {
   event: NostrEvent;
@@ -24,21 +21,8 @@ type EventSignal = {
 };
 
 type RelayState = {
-  startSubscribing: (since: number, sinceId: string) => void;
+  startSubscribing: () => void;
   stopSubscribing: () => void;
-  init: () => void;
-};
-
-const boxPosition = (box: HTMLElement, parent: HTMLElement) => {
-  if (box.getBoundingClientRect().bottom < parent.getBoundingClientRect().top) {
-    return "top";
-  } else if (
-    box.getBoundingClientRect().top > parent.getBoundingClientRect().bottom
-  ) {
-    return "bottom";
-  } else {
-    return "middle";
-  }
 };
 
 function NostrEvents() {
@@ -108,225 +92,71 @@ function NostrEvents() {
       }
     }
   };
-  const removeEventsFromTop = (n: number, relay: string) => {
-    for (let i = 0; n > 0 && i < events.length; i++) {
-      if (removeEvent(i, relay)) {
-        n--;
-      }
-    }
-  };
-  const get_rev = (
-    since: number,
-    until: number,
-    limit: number,
-    relay: string
-  ) => {
-    const event_getter = (
-      since: number,
-      until: number,
-      limit: number
-    ): Promise<EventPacket[]> => {
-      if (since === 0) {
-        console.error("since is 0");
-        return Promise.resolve([]);
-      }
-      console.log(`get ${since}..=${until}[${limit}] : ${relay}`);
-      return new Promise((resolve, reject) => {
-        const rxReq = createRxBackwardReq();
-        const events: EventPacket[] = [];
-        rxNostr.use(rxReq, { relays: [relay] }).subscribe({
-          next: (e) => {
-            events.push(e);
-          },
-          complete: () => {
-            console.log(`==`, events);
-            resolve(events);
-          },
-          error: (e) => {
-            reject(e);
-          },
-        });
-        rxReq.emit({ kinds: [7, 1], since: until, until, limit: 10_000 });
-        rxReq.emit({ kinds: [7, 1], since, until: until - 1, limit });
-        rxReq.over();
-      });
-    };
-    return get_events_rev(
-      since,
-      until,
-      limit,
-      event_getter,
-      (e) => e.event.created_at
-    );
-  };
   const relays = new Map<string, RelayState>();
   const relayState = (relay: string) => {
-    let connection: undefined | Rx.Subscription;
-    let isSubscribing = false;
+    let connection: false | Rx.Subscription = false;
     return {
-      init: () => {
-        console.log("init", relay);
-        const rxReq = createRxBackwardReq();
-        rxNostr.use(rxReq, { relays: [relay] }).subscribe(({ event, from }) => {
-          addEvent(event, from, false, true);
-        });
-        rxReq.emit({ kinds: [7, 1], limit: 10 });
-        rxReq.over();
-      },
-      startSubscribing: (since: number, sinceId: string) => {
-        if (isSubscribing) {
+      startSubscribing: () => {
+        if (connection) {
           return;
         }
-        console.log("start sub", relay);
-        isSubscribing = true;
-        (async () => {
-          const limit = 3;
-          let until = now();
-          while (isSubscribing) {
-            console.log("req es", relay);
-            until = now();
-            const es = (await get_rev(since, until, limit, relay)).filter(
-              (e) =>
-                e.event.created_at > since ||
-                (e.event.created_at == since && e.event.id < sinceId)
-            );
-            if (es.length > 0) {
-              since = es[0].event.created_at;
-              sinceId = es[0].event.id;
-            }
-            console.log(
-              relay,
-              "es =",
-              es.map((e) => e.event.id)
-            );
-            for (const e of es) {
-              addEvent(e.event, e.from, true, false);
-            }
-            if (es.length < limit) {
-              break;
-            }
-          }
-          if (isSubscribing) {
-            const rxReq = createRxForwardReq();
-            connection = rxNostr
-              .use(rxReq, { relays: [relay] })
-              .subscribe((a) => {
-                addEvent(a.event, relay, true, true);
-              });
-            rxReq.emit({ kinds: [7, 1], limit: 1_000, since: until });
-          }
-        })();
+        const rxReq = createRxForwardReq();
+        connection = rxNostr.use(rxReq, { relays: [relay] }).subscribe((a) => {
+          addEvent(a.event, relay, true, true);
+        });
+        rxReq.emit({ kinds: [7, 1], limit: 10 });
       },
       stopSubscribing: () => {
-        if (!isSubscribing) {
+        if (!connection) {
           return;
         }
         console.log("stop sub", relay);
-        isSubscribing = false;
         connection?.unsubscribe();
+        connection = false;
       },
     };
   };
 
   let ulElement: HTMLElement | null = null;
   let scrolling: undefined | number;
-  const [noteList, setNoteList] = createStore<EventSignal[]>([]);
 
   onMount(() => {
     for (const relay in rxNostr.getDefaultRelays()) {
       const s = relayState(relay);
-      s.init();
       relays.set(relay, s);
     }
     if (!ulElement) {
       return;
     }
-    const load = () => {
-      if (!ulElement) {
+    const ulElm = ulElement;
+    let isSubscribing = false;
+
+    const onscroll = () => {
+      const isSubscribingOld = isSubscribing;
+      isSubscribing = ulElm.scrollTop <= 10;
+      const start = !isSubscribingOld && isSubscribing;
+      const stop = isSubscribingOld && !isSubscribing;
+      if (!start && !stop) {
         return;
       }
-      for (const ch of Array.from(ulElement.children)) {
-        const che = ch as HTMLElement;
-        const i = events.findIndex((e) => e.event.id == che.dataset.eventId);
-        if (i === -1) {
-          continue;
-        }
-        setEvents(i, "possition", boxPosition(che, ulElement));
-      }
-      for (const [relay, state] of relays) {
-        let top = 0;
-        let middle = 0;
-        let bottom = 0;
-        const latestEvent = events.find((e) => e.relays.indexOf(relay) !== -1);
-        const [newestDate, newestId] = latestEvent
-          ? [latestEvent.event.created_at, latestEvent.event.id]
-          : [now(), ""];
-        for (const e of events) {
-          if (e.relays.indexOf(relay) === -1) {
-            continue;
-          }
-          switch (e.possition) {
-            case "top":
-              top++;
-              break;
-            case "middle":
-              middle++;
-              break;
-            case "bottom":
-              bottom++;
-              break;
-          }
-        }
-        console.log(relay, top, middle, bottom);
-        if (top < 10) {
-          state.startSubscribing(newestDate, newestId);
-        } else {
+      for (const [_relay, state] of relays) {
+        if (start) {
+          state.startSubscribing();
+        } else if (stop) {
           state.stopSubscribing();
-          if (top > 20) {
-            removeEventsFromTop(top - 10, relay);
-          }
-        }
-        if (bottom > 10) {
-          removeEventsFromBottom(bottom - 10, relay);
         }
       }
-      setNoteList(events);
-    };
-    load();
-    ulElement.onscroll = () => {
       if (scrolling) {
         clearTimeout(scrolling);
       }
-      scrolling = setTimeout(() => {
-        scrolling = undefined;
-        console.log("scrollend");
-        load();
-      }, 100);
     };
+    ulElement.onscroll = onscroll;
+    onscroll();
   });
 
   onCleanup(() => {
     webSeckets.forEach((ws) => ws.close());
   });
-
-  // const observer = new IntersectionObserver((entries) => {
-  //   for (const e of entries) {
-  //     const ev = events.findIndex(
-  //       (ev) => ev.event.id == (e.target as HTMLElement).dataset.eventId
-  //     );
-  //     if (ev === -1) {
-  //       continue;
-  //     }
-  //     if (!e.isIntersecting) {
-  //       const isDown =
-  //         ((e.rootBounds?.bottom || 0) + (e.rootBounds?.top || 0)) / 2 <
-  //         e.boundingClientRect.top;
-  //       setEvents(ev, "possition", isDown ? "bottom" : "top");
-  //     } else {
-  //       setEvents(ev, "possition", "middle");
-  //     }
-  //   }
-  // });
 
   // const cutEvents = () => {
   //   const i = events.findIndex((e) => e.possition === "middle");
@@ -338,11 +168,55 @@ function NostrEvents() {
   // let scrollHeightOld = 0;
 
   // watch events and update noteList
-  createEffect(() => {
-    trackDeep(events);
-    if (!scrolling) {
-      console.log("not scrolling");
-      setNoteList(events);
+  const observer = new IntersectionObserver((entries) => {
+    for (const e of entries) {
+      const ev = events.findIndex(
+        (ev) => ev.event.id == (e.target as HTMLElement).dataset.eventId
+      );
+      if (ev === -1) {
+        continue;
+      }
+      if (!e.isIntersecting) {
+        const isDown =
+          ((e.rootBounds?.bottom || 0) + (e.rootBounds?.top || 0)) / 2 <
+          e.boundingClientRect.top;
+        setEvents(ev, "possition", isDown ? "bottom" : "top");
+      } else {
+        setEvents(ev, "possition", "middle");
+      }
+    }
+    for (const [relay, _state] of relays) {
+      let bottom = 0;
+      let oldest = undefined;
+      for (const e of events) {
+        if (e.relays.indexOf(relay) === -1) {
+          continue;
+        }
+        if (e.possition === "bottom") {
+          bottom++;
+        }
+        oldest = e.event.created_at;
+      }
+      if (bottom > 10) {
+        removeEventsFromBottom(bottom - 10, relay);
+      } else if (bottom <= 3) {
+        const rxReq = createRxBackwardReq();
+        rxNostr.use(rxReq, { relays: [relay] }).subscribe((a) => {
+          addEvent(a.event, relay, false, false);
+        });
+        if (oldest) {
+          rxReq.emit({
+            kinds: [7, 1],
+            limit: 1_000,
+            until: oldest,
+            since: oldest,
+          });
+          rxReq.emit({ kinds: [7, 1], limit: 10, until: oldest - 1 });
+        } else {
+          rxReq.emit({ kinds: [7, 1], limit: 10 });
+        }
+        rxReq.over();
+      }
     }
   });
 
@@ -360,61 +234,38 @@ function NostrEvents() {
         // };
       }}
     >
-      <Show when={noteList.length === 0}>
+      <Show when={events.length === 0}>
         <div>loading ...</div>
       </Show>
-      <For each={noteList}>
+      <For each={events}>
         {(event) => {
           console.log("add", event.event.created_at, event.event.id);
           // if (!event.realTime && ulElement && ulElement.scrollTop === 0) {
           //   ulElement.scroll({ top: 1 });
           // }
-          return Note(event, ulElement as HTMLElement);
+          return Note(event, observer);
         }}
       </For>
     </ul>
   );
 }
 
-function Note(event: EventSignal, parent: HTMLElement) {
+function Note(event: EventSignal, observer: IntersectionObserver) {
   // onCleanup(() => {
   //   console.log("remove", event.event.created_at, event.event.id);
   // });
-  let element: HTMLElement | null = null;
-  const originalScrollTop = parent.scrollTop;
+  // let element: HTMLElement | null = null;
+  // const originalScrollTop = parent.scrollTop;
   onMount(() => {
-    if (element) {
-      if (boxPosition(element, parent) === "top") {
-        console.log("scrollby", element.getBoundingClientRect().height);
-        parent.scrollTop =
-          originalScrollTop + element.getBoundingClientRect().height;
-      }
-      // console.log(
-      //   "height",
-      //   element.getBoundingClientRect().height,
-      //   element.clientHeight,
-      //   element.offsetHeight
-      // );
-    }
+    console.log("add", event.event.created_at, event.event.id);
   });
   onCleanup(() => {
-    if (!element) {
-      return;
-    }
-    // if (boxPosition(element, parent) === "top") {
-    //   parent.scrollBy({ top: -element.getBoundingClientRect().height });
-    // }
-    console.log(
-      "height",
-      element.getBoundingClientRect().height,
-      element.clientHeight,
-      element.offsetHeight
-    );
+    console.log("remove", event.event.created_at, event.event.id);
   });
   return (
     <div
       ref={(el) => {
-        element = el;
+        observer.observe(el);
       }}
       class="grid grid-animated-ul"
       // classList={{ transition: event.transition }}
