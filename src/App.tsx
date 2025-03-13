866c4db3158be583f80f53a90f8690167e10eb0f
Mado <nanikamado@gmail.com>
2025-03-14 01:36:56 +0900

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
+import * as RxNostr from "rx-nostr";
 import "./App.css";
 import NostrEvents from "./Events.tsx";
+import { Router, Route, useParams } from "@solidjs/router";
+
+function Home() {
+  const params = useParams();
+  try {
+    const id = RxNostr.toHex(params.npub);
+    return (
+      <div class="h-dvh px-10 mx-auto grid grid-cols-1 grid-rows-[3rem_1fr] p-10">
+        <h2>Nost Events</h2>
+        <NostrEvents npub={id} />
+      </div>
+    );
+  } catch (_e) {
+    return (
+      <div class="h-dvh px-10 mx-auto grid grid-cols-1 grid-rows-[3rem_1fr] p-10">
+        <h2>Invalid npub: {params.npub}</h2>
+      </div>
+    );
+  }
+}
+
+function Usage() {
+  return (
+    <div class="h-dvh px-10 mx-auto grid grid-cols-1 grid-rows-[3rem_1fr] p-10">
+      <h2>Usage</h2>
+      <p>/:npub</p>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div class="h-dvh px-10 mx-auto grid grid-cols-1 grid-rows-[3rem_1fr] p-10">
-      <h2>Nost Events</h2>
-      <NostrEvents />
-    </div>
+    <Router>
+      <Route path="/home/:npub" component={Home} />
+      <Route path="*" component={Usage} />
+    </Router>
   );
 }
 
--- src/Events.tsx
+++ src/Events.tsx
@@ -23,9 +23,30 @@ type EventSignal = {
 type RelayState = {
   startSubscribing: () => void;
   stopSubscribing: () => void;
+  loadOldevents: (until: number | undefined) => Promise<{ success: boolean }>;
 };
 
-function NostrEvents() {
+const binarySearch = <T,>(arr: T[], f: (item: T) => boolean): number => {
+  let left = -1;
+  let right = arr.length;
+
+  while (right - left > 1) {
+    const mid = Math.floor((left + right) / 2);
+    if (f(arr[mid])) {
+      right = mid;
+    } else {
+      left = mid;
+    }
+  }
+
+  return right;
+};
+
+type NostrEventsProps = {
+  npub: string;
+};
+
+function NostrEvents({ npub }: NostrEventsProps) {
   const [events, setEvents] = createStore<EventSignal[]>([]);
 
   const webSeckets: WebSocket[] = [];
@@ -55,19 +76,19 @@ function NostrEvents() {
     realTime: boolean
   ) => {
     const e = events.findIndex((e) => e.event.id == event.id);
-    if (e !== -1) {
+    const right = binarySearch(
+      events,
+      (e) =>
+        e.event.created_at < event.created_at ||
+        (e.event.created_at === event.created_at && e.event.id > event.id)
+    );
+    if (events[right - 1]?.event.id === event.id) {
       if (events[e].relays.indexOf(relay) === -1) {
         setEvents(e, "relays", events[e].relays.length, relay);
       }
     } else {
-      const es = [
-        ...events,
-        { event: event, transition, relays: [relay], realTime },
-      ].sort((a, b) => {
-        const c =
-          [a.event.created_at, b.event.id] > [b.event.created_at, a.event.id];
-        return c ? -1 : 1;
-      });
+      const newE = { event: event, transition, relays: [relay], realTime };
+      const es = [...events.slice(0, right), newE, ...events.slice(right)];
       setEvents(es);
     }
   };
@@ -76,7 +97,6 @@ function NostrEvents() {
     const relayI = last.relays.indexOf(relay);
     if (relayI !== -1) {
       if (last.relays.length === 1) {
-        console.log("remove", events[i].event.created_at, events[i].event.id);
         setEvents(events.filter((_, j) => j !== i));
       } else {
         setEvents(i, "relays", (relays) => relays.filter((r) => r !== relay));
@@ -92,19 +112,34 @@ function NostrEvents() {
       }
     }
   };
+  let onScreenEventLowerbound = Number.MAX_SAFE_INTEGER;
+  let setFollowees: (a: string[]) => void;
+  const followees: Promise<string[]> = new Promise((resolve) => {
+    setFollowees = resolve;
+  });
   const relays = new Map<string, RelayState>();
   const relayState = (relay: string) => {
     let connection: false | Rx.Subscription = false;
+    type LoadingOldEventsStatus = {
+      until: number | undefined;
+      resolves: ((a: { success: boolean }) => void)[];
+      rejects: (() => void)[];
+    };
+    let loadingOldEvents: false | LoadingOldEventsStatus = false;
     return {
       startSubscribing: () => {
         if (connection) {
           return;
         }
-        const rxReq = createRxForwardReq();
-        connection = rxNostr.use(rxReq, { relays: [relay] }).subscribe((a) => {
-          addEvent(a.event, relay, true, true);
+        followees.then((authors) => {
+          const rxReq = createRxForwardReq();
+          connection = rxNostr
+            .use(rxReq, { relays: [relay] })
+            .subscribe((a) => {
+              addEvent(a.event, relay, true, true);
+            });
+          rxReq.emit({ kinds: [7, 1], limit: 11, authors });
         });
-        rxReq.emit({ kinds: [7, 1], limit: 10 });
       },
       stopSubscribing: () => {
         if (!connection) {
@@ -114,6 +149,73 @@ function NostrEvents() {
         connection?.unsubscribe();
         connection = false;
       },
+      loadOldevents: (until: number | undefined) => {
+        return new Promise<{ success: boolean }>((resolve, reject) => {
+          if (loadingOldEvents) {
+            console.log("skip load old", relay, until);
+            loadingOldEvents.resolves.push(resolve);
+            loadingOldEvents.rejects.push(reject);
+            return;
+          }
+          loadingOldEvents = { until, resolves: [], rejects: [] };
+          const rxReq = createRxBackwardReq();
+          console.log("load old", relay, until);
+          rxNostr.use(rxReq, { relays: [relay] }).subscribe({
+            next: (a) => {
+              addEvent(a.event, relay, false, false);
+            },
+            complete: () => {
+              const loadingOldEventsOld = loadingOldEvents;
+              loadingOldEvents = false;
+              if (loadingOldEventsOld) {
+                loadingOldEventsOld.resolves.forEach((r) =>
+                  r({ success: true })
+                );
+              }
+              resolve({ success: true });
+              return;
+            },
+            error: (e) => {
+              const loadingOldEventsOld = loadingOldEvents;
+              loadingOldEvents = false;
+              if (loadingOldEventsOld) {
+                loadingOldEventsOld.rejects.forEach((r) => r());
+              }
+              reject(e);
+              return;
+            },
+          });
+          followees.then((authors) => {
+            if (onScreenEventLowerbound !== Number.MAX_SAFE_INTEGER) {
+              rxReq.emit({
+                kinds: [7, 1],
+                limit: 10_000,
+                until: until,
+                since: onScreenEventLowerbound,
+                authors,
+              });
+            }
+            if (until) {
+              rxReq.emit({
+                kinds: [7, 1],
+                limit: 1_000,
+                until: until,
+                since: until,
+                authors,
+              });
+              rxReq.emit({
+                kinds: [7, 1],
+                limit: 8,
+                until: until - 1,
+                authors,
+              });
+            } else {
+              rxReq.emit({ kinds: [7, 1], limit: 8, authors });
+            }
+            rxReq.over();
+          });
+        });
+      },
     };
   };
 
@@ -121,6 +223,17 @@ function NostrEvents() {
   let scrolling: undefined | number;
 
   onMount(() => {
+    const rxReq = createRxForwardReq();
+    rxNostr.use(rxReq).subscribe((a) => {
+      console.log(a.event.tags);
+      const followees = a.event.tags.flatMap((t) =>
+        t.length >= 2 && t[0] === "p" ? [t[1]] : []
+      );
+      if (followees.length !== 0) {
+        setFollowees(followees);
+      }
+    });
+    rxReq.emit({ kinds: [3], limit: 1, authors: [npub] });
     for (const relay in rxNostr.getDefaultRelays()) {
       const s = relayState(relay);
       relays.set(relay, s);
@@ -168,6 +281,35 @@ function NostrEvents() {
   // let scrollHeightOld = 0;
 
   // watch events and update noteList
+  // let onScreenEventUpperbound = 0;
+
+  const load = () => {
+    for (const [relay, state] of relays) {
+      const loadRelay = () => {
+        let bottom = 0;
+        let oldest = undefined;
+        for (const e of events) {
+          if (e.relays.indexOf(relay) === -1) {
+            continue;
+          }
+          if (e.event.created_at < onScreenEventLowerbound) {
+            bottom++;
+          }
+          oldest = e.event.created_at;
+        }
+        if (bottom > 10) {
+          removeEventsFromBottom(bottom - 10, relay);
+        } else if (bottom <= 3) {
+          state.loadOldevents(oldest).then(({ success }) => {
+            if (success) {
+              loadRelay();
+            }
+          });
+        }
+      };
+      loadRelay();
+    }
+  };
   const observer = new IntersectionObserver((entries) => {
     for (const e of entries) {
       const ev = events.findIndex(
@@ -176,48 +318,18 @@ function NostrEvents() {
       if (ev === -1) {
         continue;
       }
-      if (!e.isIntersecting) {
+      if (e.isIntersecting) {
         const isDown =
           ((e.rootBounds?.bottom || 0) + (e.rootBounds?.top || 0)) / 2 <
-          e.boundingClientRect.top;
-        setEvents(ev, "possition", isDown ? "bottom" : "top");
-      } else {
-        setEvents(ev, "possition", "middle");
-      }
-    }
-    for (const [relay, _state] of relays) {
-      let bottom = 0;
-      let oldest = undefined;
-      for (const e of events) {
-        if (e.relays.indexOf(relay) === -1) {
-          continue;
-        }
-        if (e.possition === "bottom") {
-          bottom++;
-        }
-        oldest = e.event.created_at;
-      }
-      if (bottom > 10) {
-        removeEventsFromBottom(bottom - 10, relay);
-      } else if (bottom <= 3) {
-        const rxReq = createRxBackwardReq();
-        rxNostr.use(rxReq, { relays: [relay] }).subscribe((a) => {
-          addEvent(a.event, relay, false, false);
-        });
-        if (oldest) {
-          rxReq.emit({
-            kinds: [7, 1],
-            limit: 1_000,
-            until: oldest,
-            since: oldest,
-          });
-          rxReq.emit({ kinds: [7, 1], limit: 10, until: oldest - 1 });
+          (e.boundingClientRect.bottom + e.boundingClientRect.top) / 2;
+        if (isDown) {
+          onScreenEventLowerbound = events[ev].event.created_at;
         } else {
-          rxReq.emit({ kinds: [7, 1], limit: 10 });
+          // onScreenEventUpperbound = events[ev].event.created_at;
         }
-        rxReq.over();
       }
     }
+    load();
   });
 
   return (
@@ -239,7 +351,6 @@ function NostrEvents() {
       </Show>
       <For each={events}>
         {(event) => {
-          console.log("add", event.event.created_at, event.event.id);
           // if (!event.realTime && ulElement && ulElement.scrollTop === 0) {
           //   ulElement.scroll({ top: 1 });
           // }
